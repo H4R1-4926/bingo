969fbf71732f2772aaf01d91a4fac72b001af290
H4r1 <harigovindappu4926@gmail.com>
2024-08-05 12:26:31 +0530

    updated gamestate

--- lib/Application/GameBlocs/game_state.dart
+++ lib/Application/GameBlocs/game_state.dart
@@ -8,9 +8,32 @@ class GameState with _$GameState {
     required List<bool> winnibgCombs,
   }) = _GameState;
   factory GameState.initial() {
-    return GameState(
-        numbers: [],
-        isClicked: List.filled(25, false),
-        winnibgCombs: List.filled(12, false));
+    return GameState(numbers: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12',
+      '13',
+      '14',
+      '15',
+      '16',
+      '17',
+      '18',
+      '19',
+      '20',
+      '21',
+      '22',
+      '23',
+      '24',
+      '25'
+    ], isClicked: List.filled(25, false), winnibgCombs: List.filled(12, false));
   }
 }
