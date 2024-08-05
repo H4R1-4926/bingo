5bd5736565801d3f31fa3c070988c76c9d769280
H4r1 <harigovindappu4926@gmail.com>
2024-08-05 16:25:53 +0530

    added music

---
+++ lib/Infrastructure/music/music.dart
@@ -0,0 +1,24 @@
+// import 'package:audioplayers/audioplayers.dart';
+
+// class Audio {
+//   static final AudioPlayer _player = AudioPlayer();
+//   static bool _isPlaying = false;
+//   static bool _isMuted = false;
+
+//   static void playMusic() {
+//     if (!_isPlaying) {
+//       _player.setReleaseMode(ReleaseMode.loop); // Loop the audio
+//       _player.play(AssetSource('assets/music/bg.mp3'));
+//       _isPlaying = true;
+//     }
+//   }
+
+//   static void toggleMute() {
+//     _isMuted = !_isMuted;
+//     if (_isMuted) {
+//       _player.setVolume(0.0);
+//     } else {
+//       _player.setVolume(1.0);
+//     }
+//   }
+// }
