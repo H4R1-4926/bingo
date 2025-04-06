7873d4e9a4baf55b072cf0c5fca463d8f41ea8f8
H4r1 <harigovindappu4926@gmail.com>
2025-04-06 18:40:20 +0530

    changed privacy policy

--- lib/Presentation/Privacy policy/privacy_policy.dart
+++ lib/Presentation/Privacy policy/privacy_policy.dart
@@ -30,8 +30,77 @@ class PrivacyPolicyPage extends StatelessWidget {
                     fit: BoxFit.cover,
                     image: AssetImage('assets/img/bingo strips.png'))),
           ),
+          ListView(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Text('Privacy Policy',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: kWhite,
+                        fontWeight: FontWeight.bold)),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Text(
+                  'Thank you for using our app, your privacy is important us, and we want to show you how we handle your data',
+                  style: TextStyle(color: kWhite),
+                ),
+              ),
+              PPWidget(
+                title: '1. No personal data collection',
+                description:
+                    'We do not collect, styore,or share any personal data from the user. the app dows not required any information of the user to run.',
+              ),
+              PPWidget(
+                title: '2. Advertising',
+                description:
+                    'Our app may display third-party ads (such as Google Admob) to support development. These ads may collect data such ad device type and intraction with the ads to show relevent ads. We do not have any access to this information.',
+              ),
+              PPWidget(
+                title: '3. Children\'s Privacy',
+                description:
+                    'The app does not have any age limit. However , we recommand to use the app under a limited screen time',
+              ),
+              PPWidget(
+                  title: '4. Changes to This Policy',
+                  description:
+                      'We may update our Privacy policy from time to time. We will notify you of any changes by posting on update description.'),
+            ],
+          )
         ],
       ),
+    );
+  }
+}
+
+class PPWidget extends StatelessWidget {
+  final String title;
+  final String description;
+  const PPWidget({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(color: kWhite, fontSize: 22),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(description, style: const TextStyle(color: kWhite)),
+        const SizedBox(
+          height: 10,
+        ),
+      ],
     );
   }
 }
