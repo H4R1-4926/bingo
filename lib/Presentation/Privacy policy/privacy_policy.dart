a31adfa83de97160339a62806bd8da28f2c66f7d
H4r1 <harigovindappu4926@gmail.com>
2025-04-09 17:32:47 +0530

    added ad

--- lib/Presentation/Privacy policy/privacy_policy.dart
+++ lib/Presentation/Privacy policy/privacy_policy.dart
@@ -1,11 +1,15 @@
+import 'package:bingo/Application/Advertisemnet/ad_bloc.dart';
 import 'package:bingo/Core/colors/color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PrivacyPolicyPage extends StatelessWidget {
   const PrivacyPolicyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) =>
+        context.read<AdBloc>().add(const AdEvent.interstatial()));
     return Scaffold(
       backgroundColor: kblack,
       appBar: AppBar(
